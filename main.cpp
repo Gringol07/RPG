c470a3e593c549bb2604d00b767d5c96e92a91b3
Erick Alves <britoerick16@gmail.com>
2025-11-05 10:05:25 -0300

    Teste no main de uma batalha

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "src/cabecalhos/Arena.hpp"
+#include "src/cabecalhos/Personagem.hpp"
 
-int main(){
-    std::cout << "Bem vindo ao nosso RPG";
-    std::cout << "Gostaria de Jogar?";
-    std::cout << "Qual personagem?";
-    std::cout << "Clonando repositorio";
+int main()
+{
+    Arena arena = Arena();
 
+    Personagem lutador1 = Personagem("Akali", 200, 50, 70);
+    Personagem lutador2 = Personagem("Garen", 300, 70, 100);
+
+    arena.batalhar(lutador1, lutador2);
 }

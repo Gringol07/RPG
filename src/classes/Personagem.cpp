0ed0f7667e0ee2838a6abbe7fc0bb0e4549b885a
Gabriel <joaogab88hotmal@gmail.com>
2025-11-05 10:44:16 -0300

    Implementação no efetuar ação

--- src/classes/Personagem.cpp
+++ src/classes/Personagem.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "../cabecalhos/Personagem.hpp"
+#include <random>
+std::random_device rd; // Semente
+std::mt19937 gen(rd()); // Motor de geração
+std::uniform_int_distribution<> distrib(1,2); // Distribuição (números de 1 a 100)
+int fugir = distrib(gen);
 
 Personagem::Personagem(std::string nome, int vida, int ataque, int defesa)
 {
@@ -20,8 +25,26 @@ bool Personagem::estaVivo()
 }
 
 int Personagem::efetuarAcao()
-{
-    return ataque;
+{   int acao;
+    std:: cout << "Escolha uma ação: 1-Atacar\n2-Defender\n3-Fugir\n ";
+    std:: cin >> acao;
+    if (acao == 1){
+        return ataque;
+    }
+    else if (acao == 2){
+        return defesa;
+    }
+    else if(acao == 3){
+        if (fugir == 1){
+            std:: cout <<"Você conseguiu fugir!";
+            return 0;
+        }
+        else {
+            std:: cout << "Tentativa de fuga falhou! a luta continua";
+            return -1;
+        }
+        return fugir;
+    }
 }
 
 void Personagem::receberDano(int dano)
